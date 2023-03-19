7a343260327469cae2ffa04b0edaf011326ae60f
PC32-IFLAB <127027989+syahransyahputra@users.noreply.github.com>
2023-03-20 06:54:48 +0700

    first commit

---
+++ jurnal5_1302210113/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal5_1302210113
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
