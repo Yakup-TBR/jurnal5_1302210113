65473962944e9b2597a705d234f9c470f689febd
PC32-IFLAB <127027989+syahransyahputra@users.noreply.github.com>
2023-03-20 07:17:59 +0700

    Update Penjumlahan.cs
    menambahkan class Penjumlahan dengan method  JumlahTigaAngka

--- jurnal5_1302210113/Penjumlahan.cs
+++ jurnal5_1302210113/Penjumlahan.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace jurnal5_1302210113
 {
-    internal class Class1
+    internal class Penjumlahan
     {
+        public static void PenjumlahanTigaAngka<T>(T a, T b, T c)
+        {
+
+            Console.WriteLine("" + a + "" + b + "" + c);
+
+
+
+        }
     }
 }
